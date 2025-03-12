ed537b9d11b97cc48637943e3180a0309055806f
dikiiwahyudii11 <dikiiwahyudii11@gmail.com>
2025-03-12 10:57:38 +0700

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -25,7 +25,7 @@ export function addToCart(productId) {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
-  })
+  });
 
   if (matchingItem) {
     matchingItem.quantity += 1;
@@ -50,6 +50,20 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach(cartItem => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,10 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayJS from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../data/deliveryOptions.js';
+
+const today = dayJS();
 
 let cartSummaryHTML = '';
 
@@ -96,7 +98,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     html +=
     `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
         <input type="radio" ${isChecked ? 'checked': ''}
           class="delivery-option-input"
           name="delivery-option-${matchingProduct.id}">
@@ -126,3 +128,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   });
 });
 
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const {productId, deliveryOptionId} = element.dataset; 
+    updateDeliveryOption(productId, deliveryOptionId);
+  })
+});
