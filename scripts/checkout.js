24bc46c07e300ca991a0c0297c1881d86736d5fd
dikiiwahyudii11 <dikiiwahyudii11@gmail.com>
2025-03-13 15:39:48 +0700

    load product in before testing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,10 @@
+import { loadProducts } from "../data/products.js";
 import {renderOrderSummary} from "./checkout/orderSummary.js"; 
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js'
-renderOrderSummary();
-renderPaymentSummary();
+// import '../data/backend-practice.js'
+
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
