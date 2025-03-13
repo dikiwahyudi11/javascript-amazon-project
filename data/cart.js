725d1cb538db9a864cd2b6cb6aec3401a6b21a99
dikiiwahyudii11 <dikiiwahyudii11@gmail.com>
2025-03-13 16:03:18 +0700

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -71,3 +71,16 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
   saveToStorage();
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log('load cart');
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+
+}
